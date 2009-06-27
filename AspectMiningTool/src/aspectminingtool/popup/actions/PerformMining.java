e191f1a2723cfe0d129a9879c363f0ca12a953e1
luciamasola <luciamasola@3faa3960-6280-11de-987a-95e5a5a5d5f1>
2009-06-27 02:39:03 +0000

    Se modifica la clase para poder recorrer todo un proyecto.

--- AspectMiningTool/src/aspectminingtool/popup/actions/PerformMining.java
+++ AspectMiningTool/src/aspectminingtool/popup/actions/PerformMining.java
@@ -1,7 +1,15 @@
 package aspectminingtool.popup.actions;
 
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IPackageFragment;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.IObjectActionDelegate;
@@ -34,13 +42,42 @@ public class PerformMining implements IObjectActionDelegate {
 	 * @see IActionDelegate#run(IAction)
 	 */
 	public void run(IAction action) {
-		
+		/*
 		if (selection instanceof IStructuredSelection) {
 			ICompilationUnit lwUnit = (ICompilationUnit) ((IStructuredSelection) selection)
 					.getFirstElement();
 			ASTInspectorProject AIP = new ASTInspectorProject(); 
 			AIP.run(lwUnit); // devuelve un AbstractASTArticle y a este se le ejecuta el run(lwUnit)
 		}
+		*/
+				
+		if (selection instanceof IStructuredSelection) {
+			
+			IProject project = (IProject) ((IStructuredSelection) selection).getFirstElement();
+						
+			IJavaProject javaProject = JavaCore.create(project);
+							
+			try {
+				IPackageFragment[] fragments = javaProject.getPackageFragments();
+				for (int i=0;i<fragments.length;i++){
+					System.out.println("paquete: "+ fragments[i].getElementName());
+					ICompilationUnit[] compUnit = fragments[i].getCompilationUnits();
+					for(int y=0;y<compUnit.length;y++){
+						System.out.println("compUnit: "+ compUnit[y].getElementName());
+						ASTInspectorProject AIP = new ASTInspectorProject(); 
+						AIP.run(compUnit[y]);
+					}
+					
+				}
+			} catch (JavaModelException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+		}
+		else
+			System.out.println("no es una IStructuredSelection");
 		
 	}
 

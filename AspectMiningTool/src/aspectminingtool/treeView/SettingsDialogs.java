341fa9c1384755b519eddbdd0df6705f7e0cbf37
luciamasola <luciamasola@3faa3960-6280-11de-987a-95e5a5a5d5f1>
2009-08-02 00:43:05 +0000

    Cambio en el tipo de Dialogo. Se cambia a dialogo  SWT.

--- AspectMiningTool/src/aspectminingtool/treeView/SettingsDialogs.java
+++ AspectMiningTool/src/aspectminingtool/treeView/SettingsDialogs.java
@@ -1,100 +1,159 @@
 package aspectminingtool.treeView;
 
-
 import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.viewers.CheckStateChangedEvent;
-import org.eclipse.jface.viewers.ICheckStateListener;
 import org.eclipse.jface.viewers.CheckboxTreeViewer;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.jface.viewers.ICheckStateListener;
+
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CLabel;
 
 
 
 
+/**
+* This code was edited or generated using CloudGarden's Jigloo
+* SWT/Swing GUI Builder, which is free for non-commercial
+* use. If Jigloo is being used commercially (ie, by a corporation,
+* company or business for any purpose whatever) then you
+* should purchase a license for each developer using Jigloo.
+* Please visit www.cloudgarden.com for details.
+* Use of Jigloo implies acceptance of these licensing terms.
+* A COMMERCIAL LICENSE HAS NOT BEEN PURCHASED FOR
+* THIS MACHINE, SO JIGLOO OR THIS CODE CANNOT BE USED
+* LEGALLY FOR ANY CORPORATE OR COMMERCIAL PURPOSE.
+*/
+public class SettingsDialogs extends org.eclipse.swt.widgets.Dialog {
 
-public class SettingsDialogs extends Dialog {
-	
+	private Shell dialogShell;
+	private CLabel selectLabel;
+	private Button cancelButton;
+	private Button acceptButton;
 	private IJavaProject project;
-	
-	public SettingsDialogs(Shell parentShell, IJavaProject javaProject) {
-		super(parentShell);
-		this.project = javaProject; 
+	private static CheckboxTreeViewer treeViewer1;
+
+	/**
+	* Auto-generated main method to display this 
+	* org.eclipse.swt.widgets.Dialog inside a new Shell.
+	*/
+	public static void main(String[] args) {
+		try {
+			Display display = Display.getDefault();
+			Shell shell = new Shell(display);
+			SettingsDialogs inst = new SettingsDialogs(shell, SWT.NULL, null);
+			inst.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
-	protected void configureShell(Shell shell) {
-	    super.configureShell(shell);
-	    // Set the title bar text and the size
-	    shell.setText("File Tree");
-	    shell.setSize(500,500);
-	   	 
-	  }
-	
-	
-	protected Control createDialogArea(Composite parent) {
-		
-			Composite composite = (Composite)super.createDialogArea(parent);
-//			Composite composite = new Composite(parent, SWT.BORDER);
-		    composite.setLayout(new GridLayout(1,false));
-		    
-		    Composite composite1 = new Composite(composite,SWT.BORDER);
-		    composite1.setLayout(new GridLayout(1,false));
-		    composite1.setLocation(200, 200);
-		    Button boton1 = new Button(composite1,SWT.BORDER_DASH);
-		    boton1.setText("Botón 1");
-		    
+	public SettingsDialogs(Shell parent, int style, IJavaProject project) {
+		super(parent, style);
+		this.project = project;
+	}
 
-		    // Create the tree viewer to display the file tree
-		   // final TreeViewer tv = new TreeViewer(composite);
-		    final CheckboxTreeViewer tv = new CheckboxTreeViewer(composite, SWT.BORDER );
-		    tv.getTree().setLayoutData(new GridData(GridData.FILL_BOTH));
-		    tv.setContentProvider(new ProjectTreeContentProvider(project));
-		    tv.setLabelProvider(new ProjectTreeLabelProvider());
-		    tv.setInput("root"); // pass a non-null that will be ignored
+	public void open() {
+		try {
+			Shell parent = getParent();
+			dialogShell = new Shell(parent, SWT.DIALOG_TRIM | SWT.APPLICATION_MODAL);
+			dialogShell.setText("Filter");
 
-		    // When user checks a checkbox in the tree, check all its children
-		    tv.addCheckStateListener(new ICheckStateListener() {
-		      public void checkStateChanged(CheckStateChangedEvent event) {
+			dialogShell.setLayout(new FormLayout());
+			{
+				treeViewer1 = new CheckboxTreeViewer(dialogShell, SWT.NONE);
+				FormData treeViewer1LData = new FormData();
+				treeViewer1LData.width = 298;
+				treeViewer1LData.height = 240;
+				treeViewer1LData.left =  new FormAttachment(0, 1000, 17);
+				treeViewer1LData.top =  new FormAttachment(0, 1000, 39);
+				treeViewer1.getControl().setLayoutData(treeViewer1LData);
+				treeViewer1.setContentProvider(new ProjectTreeContentProvider(project));
+				treeViewer1.setLabelProvider(new ProjectTreeLabelProvider());
+				treeViewer1.setInput("root");
+			}
+			{
+				acceptButton = new Button(dialogShell, SWT.PUSH | SWT.CENTER);
+				acceptButton.setText("Aceptar");
+				FormData acceptButtonLData = new FormData(64, 25);
+				acceptButtonLData.width = 64;
+				acceptButtonLData.height = 25;
+				acceptButtonLData.bottom =  new FormAttachment(1000, 1000, -7);
+				acceptButtonLData.right =  new FormAttachment(1000, 1000, -84);
+				acceptButton.setLayoutData(acceptButtonLData);
+				acceptButton.setSize(64, 25);
+				acceptButton.setBounds(245, 253, 53, 25);
+			}
+			{
+				cancelButton = new Button(dialogShell, SWT.PUSH | SWT.CENTER);
+				cancelButton.setText("Cancelar");
+				FormData cancelButtonLData = new FormData();
+				cancelButtonLData.width = 64;
+				cancelButtonLData.height = 25;
+				cancelButtonLData.bottom =  new FormAttachment(1000, 1000, -7);
+				cancelButtonLData.right =  new FormAttachment(1000, 1000, -14);
+				cancelButton.setLayoutData(cancelButtonLData);
+			}
+			{
+				selectLabel = new CLabel(dialogShell, SWT.NONE);
+				selectLabel.setText("Seleccione las clases a excluir del análisis:");
+				FormData selectLabelLData = new FormData();
+				selectLabelLData.width = 222;
+				selectLabelLData.height = 21;
+				selectLabelLData.left =  new FormAttachment(0, 1000, 12);
+				selectLabelLData.top =  new FormAttachment(0, 1000, 12);
+				selectLabel.setLayoutData(selectLabelLData);
+			}
 
-		          // . . . checks or unchekcs all its children
-		          tv.setSubtreeChecked(event.getElement(), event.getChecked());
-		 		 ProjectTreeContentProvider projectTreeContentProvider = (ProjectTreeContentProvider) tv.getContentProvider();
-		          checkPath(event.getElement(),tv,projectTreeContentProvider);
-		          
-		      }
-		    });
-		 
-		    
-		    return composite;
-	   }
+			treeViewer1.addCheckStateListener(new ICheckStateListener() {
+			      public void checkStateChanged(CheckStateChangedEvent event) {
+
+			          // . . . checks or unchekcs all its children
+			    	  treeViewer1.setSubtreeChecked(event.getElement(), event.getChecked());
+			 		 ProjectTreeContentProvider projectTreeContentProvider = (ProjectTreeContentProvider) treeViewer1.getContentProvider();
+			         checkPath(event.getElement(),treeViewer1,projectTreeContentProvider);
+			          
+			      }
+			    });
+			
+			dialogShell.layout();
+			dialogShell.pack();	
+			dialogShell.setSize(432, 418);
+			dialogShell.setLocation(getParent().toDisplay(100, 100));
+			dialogShell.open();
+			Display display = dialogShell.getDisplay();
+			while (!dialogShell.isDisposed()) {
+				if (!display.readAndDispatch())
+					display.sleep();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 	
-	static void checkPath(Object element, CheckboxTreeViewer tv, ProjectTreeContentProvider projectTreeContentProvider) {
+	static void checkPath(Object element, CheckboxTreeViewer treeViewer1, ProjectTreeContentProvider projectTreeContentProvider) {
 	    
 
-         Object parentElement = projectTreeContentProvider.getParent(element);
-         if (parentElement == null) return;
-         int index = 0;
-         Object[] children = projectTreeContentProvider.getChildren(parentElement);
-         boolean check = true;
-         while (index < children.length){
-        	 if (!tv.getChecked(children[index])){
-        		check = false;
-        		break;
-        	 }
-        	 index++;
-         }
-         tv.setChecked(parentElement, check);
-         checkPath(parentElement,tv,projectTreeContentProvider);
-         
+        Object parentElement = projectTreeContentProvider.getParent(element);
+        if (parentElement == null) return;
+        int index = 0;
+        Object[] children = projectTreeContentProvider.getChildren(parentElement);
+        boolean check = true;
+        while (index < children.length){
+       	 if (!treeViewer1.getChecked(children[index])){
+       		check = false;
+       		break;
+       	 }
+       	 index++;
+        }
+        treeViewer1.setChecked(parentElement, check);
+        checkPath(parentElement,treeViewer1,projectTreeContentProvider);
+        
 	}
-
-
-
 	
-
 }

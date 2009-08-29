2f05a99069bfb722ab9528f3cf7f99313c02945e
luciamasola <luciamasola@3faa3960-6280-11de-987a-95e5a5a5d5f1>
2009-08-29 01:55:00 +0000

    Se modifica la clase para agregar el Dialogo de progreso.

--- AspectMiningTool/src/aspectminingtool/parser/ProjectInspector.java
+++ AspectMiningTool/src/aspectminingtool/parser/ProjectInspector.java
@@ -1,11 +1,18 @@
 package aspectminingtool.parser;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.IPackageFragment;
 import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jface.dialogs.ProgressMonitorDialog;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.swt.widgets.Shell;
 
 import aspectminingtool.data.Fact;
 
@@ -16,37 +23,58 @@ import aspectminingtool.data.Fact;
  * @author maria
  *
  */
-public class ProjectInspector{
+public class ProjectInspector implements IRunnableWithProgress{
 
-
+	private ArrayList facts = new ArrayList();
+	private IJavaProject javaProject;
+	private FactsVisitor visitor;
 	
-	public ProjectInspector(){
+	public ProjectInspector(IJavaProject javaProject, FactsVisitor visitor){
+		this.javaProject = javaProject;
+		this.visitor = visitor;
 	}
-	                     
+	
+	public ArrayList getFacts(){
+		return facts;
+	}
+	
+
+	public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
 		
-	public ArrayList getProjectFacts(IJavaProject javaProject, FactsVisitor visitor){
-		
-		ArrayList facts = new ArrayList();
-		
+		monitor.beginTask("Executing Analysis...", IProgressMonitor.UNKNOWN);
+        
 		try {
+
 			IPackageFragment[] fragments = javaProject.getPackageFragments();
-			
-			for (int i=0 ; i < fragments.length ; i++){
-				ICompilationUnit[] compUnit = fragments[i].getCompilationUnits();
-				
-				for(int y=0 ; y < compUnit.length ; y++){
-					ASTClassInspector AIP = new ASTClassInspector(); 
+								
+			for (int i = 0; i < fragments.length; i++) {
+				ICompilationUnit[] compUnit = fragments[i]
+						.getCompilationUnits();
+
+				for (int y = 0; y < compUnit.length; y++) {
+					ASTClassInspector AIP = new ASTClassInspector();
 					facts.addAll(AIP.getCompilationFacts(compUnit[y],visitor));
+					
+					if (monitor.isCanceled())
+						//System.out.println("cancel");
+						throw new InterruptedException("The long running operation was cancelled");
 				}
-				
+
 			}
 		} catch (JavaModelException e) {
 			e.printStackTrace();
 		}
-		return facts;
 		
+        monitor.done();
+      //  return Status.OK_STATUS;
+			
 	}
-	
-	
-	
+			    	
+			
+		
 }
+	
+	
+	
+		
+

b95c1729a20a8c593ee2319a7d047c402c7a7cee
luciamasola <luciamasola@3faa3960-6280-11de-987a-95e5a5a5d5f1>
2009-09-08 04:09:52 +0000

    Se agrega función que devuelve un recurso dado un nombre.

--- AspectMiningTool/src/JessIntegrationModel/ProjectModel.java
+++ AspectMiningTool/src/JessIntegrationModel/ProjectModel.java
@@ -1,6 +1,11 @@
 package JessIntegrationModel;
 
+import org.eclipse.core.resources.IResource;
+import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IPackageFragment;
+import org.eclipse.jdt.core.JavaModelException;
+
 
 public class ProjectModel {
 
@@ -22,6 +27,42 @@ public class ProjectModel {
 	
 	public String getName(){
 		return name;
+	}
+	
+	/**
+	 * Returns the resource associated with the parameter name
+	 * @param name
+	 * @return
+	 */
+	public IResource getAssociatedResource(String name){
+		
+		name = name.replace(".", "/");
+		
+		IPackageFragment[] fragments;
+		try {
+			fragments = project.getPackageFragments();
+			for (int i = 0; i < fragments.length; i++) {
+				
+				
+				ICompilationUnit[] compUnit = fragments[i].getCompilationUnits();
+
+				for (int y = 0; y < compUnit.length; y++) {
+									
+					String path = compUnit[y].getPath().toString(); 
+					if (path.contains(name)){
+						IResource resource = compUnit[y].getCorrespondingResource();
+						return resource;
+					}
+				}
+
+			}
+		} catch (JavaModelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return null;
+		
 	}
 	
 	

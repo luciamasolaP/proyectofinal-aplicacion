cdff8a25306261ed0605d3601aedf4efeab9573a
luciamasola <luciamasola@3faa3960-6280-11de-987a-95e5a5a5d5f1>
2009-09-08 04:09:25 +0000

    Se crean la view para UniqueMethods

---
+++ AspectMiningTool/src/aspectminingtool/views/UniqueMethods/UniqueMethodsContentProvider.java
@@ -0,0 +1,74 @@
+package aspectminingtool.views.UniqueMethods;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.eclipse.jface.viewers.IStructuredContentProvider;
+import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.jface.viewers.Viewer;
+
+import aspectminingtool.JessIntegrationModel.FanIn.Call_Counted;
+import aspectminingtool.JessIntegrationModel.FanIn.FanInModel;
+import aspectminingtool.JessIntegrationModel.FanIn.Fan_in_Result;
+
+public class UniqueMethodsContentProvider implements IStructuredContentProvider {
+
+	private FanInModel fanInModel = null;
+	
+//	@Override
+//	public Object[] getChildren(Object parentElement) {
+//		
+//		if (parentElement instanceof Fan_in_Result){
+//			String key = ((Fan_in_Result)parentElement).getMetodo().getId();
+//			ArrayList<Call_Counted> hijos = (ArrayList<Call_Counted>) fanInModel.getCalls().get(key);
+//			if (hijos != null)
+//				return hijos.toArray();
+//			
+//		}
+//		
+//		return null;
+//	}
+//
+//	@Override
+//	public Object getParent(Object element) {
+//		
+//		if (element instanceof Call_Counted){ 
+//			String cc = ((Call_Counted)element).getCalle_id();
+//			for (Iterator<Fan_in_Result> i = fanInModel.getResultadoFanIn().iterator(); i.hasNext() ;){
+//				Fan_in_Result parent = i.next(); 
+//				if (parent.getMetodo().getId().equals(cc))
+//						return parent;
+//			}
+//		}
+//		
+//		return null;
+//	}
+//
+//	@Override
+//	public boolean hasChildren(Object element) {
+//		Object[] obj = getChildren(element);
+//
+//	    // Return whether the parent has children
+//	    return obj == null ? false : obj.length > 0;
+//	}
+
+	@Override
+	public Object[] getElements(Object inputElement) {
+		fanInModel = (FanInModel)inputElement;
+		return fanInModel.getResultadoFanIn().toArray();
+
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

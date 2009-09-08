13cc31352c5fd0e7a3b265c79dfaa059f4e0ccac
luciamasola <luciamasola@3faa3960-6280-11de-987a-95e5a5a5d5f1>
2009-09-08 22:50:52 +0000

    Modelo de Graph Flow.

---
+++ AspectMiningTool/src/aspectminingtool/JessIntegrationModel/FlowGraph/FlowGraphModel.java
@@ -0,0 +1,25 @@
+package aspectminingtool.JessIntegrationModel.FlowGraph;
+
+import aspectminingtool.InferenceEngine.InferenceEngine;
+import JessIntegrationModel.IResultsModel;
+import JessIntegrationModel.ProjectModel;
+
+public class FlowGraphModel implements IResultsModel {
+
+	public FlowGraphModel(ProjectModel pm, InferenceEngine inferenceEngine) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ProjectModel getProjectModel() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

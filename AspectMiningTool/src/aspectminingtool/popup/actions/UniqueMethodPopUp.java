005f4ad25b8330eb98e7c6d3235fd3372887f08b
luciamasola <luciamasola@3faa3960-6280-11de-987a-95e5a5a5d5f1>
2009-09-08 23:13:17 +0000

    Se crea el popUp para Unique Methods.

--- AspectMiningTool/src/aspectminingtool/popup/actions/UniqueMethodPopUp.java
+++ AspectMiningTool/src/aspectminingtool/popup/actions/UniqueMethodPopUp.java
@@ -2,10 +2,16 @@ package aspectminingtool.popup.actions;
 
 
 
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+
 import JessIntegrationModel.IResultsModel;
 import aspectminingtool.Algorithms.Algorithm;
 import aspectminingtool.Algorithms.UniqueMethodsAlgorithm;
 import aspectminingtool.InferenceEngine.InferenceEngine;
+import aspectminingtool.JessIntegrationModel.UniqueMethods.UniqueMethodsModel;
+import aspectminingtool.views.UniqueMethods.ViewPartUniqueMethods;
+
 
 
 public class UniqueMethodPopUp extends AbstractPerformMiningPopup {
@@ -21,15 +27,33 @@ public class UniqueMethodPopUp extends AbstractPerformMiningPopup {
 	}
 
 	@Override
-	protected void showResults(IResultsModel results2) {
-		// TODO Auto-generated method stub
-		
+	protected void showResults(IResultsModel results) {
+		try {
+
+//			ViewPartFanIn view = (ViewPartFanIn) PlatformUI.getWorkbench().getActiveWorkbenchWindow()
+//			.getActivePage().showView(ViewPartFanIn.ID_VIEW,results.getId() , IWorkbenchPage.VIEW_CREATE);
+//			para abrir en multiples vistas, esto y ***			
+
+			ViewPartUniqueMethods view = (ViewPartUniqueMethods) PlatformUI.getWorkbench().getActiveWorkbenchWindow()
+			.getActivePage().showView(ViewPartUniqueMethods.ID_VIEW );
+			
+			view.setModel(results);
+
+//			PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().showView(ViewPartFanIn.ID_VIEW,results.getId() , IWorkbenchPage.VIEW_ACTIVATE);
+//			**			
+			
+		} catch (PartInitException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Override
-	protected IResultsModel getResults(InferenceEngine InferenceEngine) {
-		// TODO Auto-generated method stub
-		return null;
+	protected IResultsModel getResults(InferenceEngine inferenceEngine) {
+
+
+		return new UniqueMethodsModel(pm, inferenceEngine);
+
 	}
 
 	

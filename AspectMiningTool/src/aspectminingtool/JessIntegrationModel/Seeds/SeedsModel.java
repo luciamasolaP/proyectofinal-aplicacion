e103b5cabce2d2ef08663c6f488b60b0cf33cfa9
luciamasola <luciamasola@3faa3960-6280-11de-987a-95e5a5a5d5f1>
2009-09-09 03:46:17 +0000

    Modelo para la vista de Seeds.

---
+++ AspectMiningTool/src/aspectminingtool/JessIntegrationModel/Seeds/SeedsModel.java
@@ -0,0 +1,102 @@
+package aspectminingtool.JessIntegrationModel.Seeds;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import JessIntegrationModel.IResultsModel;
+import JessIntegrationModel.ProjectModel;
+import aspectminingtool.JessIntegrationModel.FanIn.Call_Counted;
+
+public class SeedsModel implements IResultsModel{
+
+	Map<String,List<Call_Counted>> calls;
+	List<Description> description;
+	ProjectModel projectModel;
+	
+	
+	public SeedsModel(Map<String,List<Call_Counted>> calls, List<Description> description, ProjectModel pm) {
+		super();
+		this.calls = calls;
+		this.projectModel = pm;
+		this.description = description;
+		
+	}
+
+	public SeedsModel(ProjectModel pm) {
+		this.calls = new HashMap<String,List<Call_Counted>>();
+		this.projectModel = pm;
+		this.description = new ArrayList<Description>();
+		//contructModel(pm);
+	}
+	
+	public List<Description> getDescription() {
+		return description;
+	}
+
+	public void setDescription(List<Description> description) {
+		this.description = description;
+	}
+
+	public Map<String,List<Call_Counted>> getCalls() {
+		return calls;
+	}
+
+	public void setCalls(Map<String,List<Call_Counted>> calls) {
+		this.calls = calls;
+	}
+
+
+//	public void addCallCounted(Call_Counted cc){
+//		String id = cc.getCalle_id();
+//		List<Call_Counted> call = calls.get(id);
+//
+//		if (call == null){
+//			call = new ArrayList<Call_Counted>();
+//		}
+//		
+//		call.add(cc);
+//		
+//		calls.remove(id);
+//		calls.put(id, call);
+//	}
+
+	@Override
+	public String getId() {
+		return projectModel.getName();
+	}
+
+
+	public ProjectModel getProjectModel() {
+		return projectModel;
+	}
+
+	public void setProjectModel(ProjectModel projectModel) {
+		this.projectModel = projectModel;
+	}
+	
+	
+//	private void contructModel(ProjectModel projectModel){
+//		
+//		setProjectModel(projectModel);
+//		
+//		//setDescriptions(constructDescription());
+//		setCalls(contructCalls());
+//
+//		
+//	}
+
+
+//	private Object constructDescription() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+
+//	private Map<String, List<Call_Counted>> contructCalls() {
+//		
+//		return calls;
+//	}
+
+	
+}

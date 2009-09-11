f5e19d167053d83620251e91af031cc191a77134
luciamasola <luciamasola@3faa3960-6280-11de-987a-95e5a5a5d5f1>
2009-09-11 00:45:49 +0000

    Vista de GraphFlow.

---
+++ AspectMiningTool/src/aspectminingtool/views/FlowGraph/ViewPartFlowGraph.java
@@ -0,0 +1,54 @@
+package aspectminingtool.views.FlowGraph;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+
+/**
+* This code was edited or generated using CloudGarden's Jigloo
+* SWT/Swing GUI Builder, which is free for non-commercial
+* use. If Jigloo is being used commercially (ie, by a corporation,
+* company or business for any purpose whatever) then you
+* should purchase a license for each developer using Jigloo.
+* Please visit www.cloudgarden.com for details.
+* Use of Jigloo implies acceptance of these licensing terms.
+* A COMMERCIAL LICENSE HAS NOT BEEN PURCHASED FOR
+* THIS MACHINE, SO JIGLOO OR THIS CODE CANNOT BE USED
+* LEGALLY FOR ANY CORPORATE OR COMMERCIAL PURPOSE.
+*/
+public class ViewPartFlowGraph extends ViewPart {
+    public static final String ID_VIEW =
+        "aspectminingtool.views.FlowGraph.ViewPartFlowGraph"; //$NON-NLS-1$
+     
+
+    /**
+     * 
+     */
+    public ViewPartFlowGraph() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /* (non-Javadoc)
+     * @see org.eclipse.ui.IWorkbenchPart#createPartControl(org.eclipse.swt.widgets.Composite)
+     */
+    public void createPartControl(Composite parent) {
+        
+
+    }
+
+    /* (non-Javadoc)
+     * @see org.eclipse.ui.IWorkbenchPart#setFocus()
+     */
+    public void setFocus() {
+        // TODO Auto-generated method stub
+    }
+    
+    /**
+     * Cleans up all resources created by this ViewPart.
+     */
+    public void dispose() {
+        super.dispose();
+    }
+    
+}

21eeb2c1685c129a052cc30867b7d8601310a0d9
luciamasola <luciamasola@3faa3960-6280-11de-987a-95e5a5a5d5f1>
2009-09-11 00:44:33 +0000

    Se cambia el layout.

--- AspectMiningTool/src/aspectminingtool/dialogs/SettingsDialogs.java
+++ AspectMiningTool/src/aspectminingtool/dialogs/SettingsDialogs.java
@@ -5,27 +5,21 @@ import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jface.viewers.CheckStateChangedEvent;
 import org.eclipse.jface.viewers.CheckboxTreeViewer;
 import org.eclipse.jface.viewers.ICheckStateListener;
-
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CLabel;
+import org.eclipse.swt.custom.CTabFolder;
+import org.eclipse.swt.custom.CTabItem;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.FillLayout;
-import org.eclipse.swt.layout.FormAttachment;
-import org.eclipse.swt.layout.FormData;
-import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.custom.CLabel;
-import org.eclipse.swt.custom.CTabFolder;
-import org.eclipse.swt.custom.CTabItem;
-import org.eclipse.swt.custom.SashForm;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.graphics.Color;
 
 /**
  * This code was edited or generated using CloudGarden's Jigloo SWT/Swing GUI
@@ -43,27 +37,29 @@ public class SettingsDialogs extends org.eclipse.swt.widgets.Dialog {
 	private Button okButton;
 	private CTabFolder cTabFolder1;
 	private CTabItem cTabItem1;
-	private CLabel labelLlamadores;
-	private Button FilterGetersSetter;
-	private Label Umbral;
-	private Text umbralText;
-	private Composite composite6;
+	private Group composite6;
 	private Composite composite5;
-	private SashForm sashForm1;
-	private Composite composite4;
 	private Composite composite3;
 	private Button acceptButton;
 	static private CheckboxTreeViewer treeViewerLlamadoras;
-	private Composite composite1;
+	private Group groupLeft;
+	private Group composite1;
+	private CLabel labelUmbral;
+	private Composite compositeUmbral;
+	private Text Umbral;
+	private Button filterGetSet;
+	private Group groupCallees;
 	private Button cancelButton;
 	static private CheckboxTreeViewer treeViewerLlamadas;
-	private CLabel labelLlamados;
-	private Composite composite2;
 	private Button CancelButton;
 	private CTabItem cTabItem2;
 
 	private IJavaProject project;
 
+	public SettingsDialogs(Shell parent, int style) {
+		super(parent,style);
+	}
+	
 	public SettingsDialogs(Shell parent, int style, IJavaProject project) {
 		super(parent, style);
 		this.project = project;
@@ -82,172 +78,161 @@ public class SettingsDialogs extends org.eclipse.swt.widgets.Dialog {
 			//	SWTResourceManager.registerResourceUser(dialogShell);
 			}
 			
-			FillLayout dialogShellLayout = new FillLayout(org.eclipse.swt.SWT.HORIZONTAL);
+			GridLayout dialogShellLayout = new GridLayout();
+			dialogShellLayout.makeColumnsEqualWidth = true;
 			dialogShell.setLayout(dialogShellLayout);
 			dialogShell.setText("Results Filters");
 			dialogShell.setDragDetect(false);
-			dialogShell.setSize(625, 368);
 			{
-				composite6 = new Composite(dialogShell, SWT.NONE);
+				composite1 = new Group(dialogShell, SWT.NONE);
+				composite1.setText("Configuracin");
+				GridLayout composite1Layout = new GridLayout();
+				composite1Layout.numColumns = 2;
+				GridData composite1LData = new GridData();
+				composite1LData.horizontalAlignment = GridData.FILL;
+				composite1LData.grabExcessHorizontalSpace = true;
+				composite1LData.heightHint = 37;
+				composite1.setLayoutData(composite1LData);
+				composite1.setLayout(composite1Layout);
+				{
+					GridData compositeUmbralLData = new GridData();
+					compositeUmbralLData.horizontalAlignment = GridData.FILL;
+					compositeUmbralLData.heightHint = 31;
+					compositeUmbral = new Composite(composite1, SWT.NONE);
+					GridLayout compositeUmbralLayout = new GridLayout();
+					compositeUmbralLayout.makeColumnsEqualWidth = true;
+					compositeUmbralLayout.numColumns = 2;
+					compositeUmbral.setLayout(compositeUmbralLayout);
+					compositeUmbral.setLayoutData(compositeUmbralLData);
+					{
+						labelUmbral = new CLabel(compositeUmbral, SWT.NONE);
+						GridData labelUmbralLData = new GridData();
+						labelUmbralLData.horizontalIndent = -5;
+						labelUmbralLData.widthHint = 47;
+						labelUmbralLData.heightHint = 21;
+						labelUmbral.setLayoutData(labelUmbralLData);
+						labelUmbral.setText("Umbral");
+					}
+					{
+						Umbral = new Text(compositeUmbral, SWT.BORDER);
+						GridData UmbralLData = new GridData();
+						UmbralLData.widthHint = 24;
+						UmbralLData.heightHint = 15;
+						Umbral.setLayoutData(UmbralLData);
+						Umbral.setText("0");
+					}
+				}
+				{
+					filterGetSet = new Button(composite1, SWT.CHECK | SWT.LEFT);
+					GridData filterGetSetLData = new GridData();
+					filterGetSet.setLayoutData(filterGetSetLData);
+					filterGetSet.setText("Filtrar métodos Getters y Setters");
+				}
+			}
+			{
+				composite6 = new Group(dialogShell, SWT.NONE);
+				composite6.setText("Filtrado");
 				GridLayout composite6Layout = new GridLayout();
 				composite6Layout.makeColumnsEqualWidth = true;
+				GridData composite6LData = new GridData();
+				composite6LData.horizontalAlignment = GridData.FILL;
+				composite6LData.grabExcessHorizontalSpace = true;
+				composite6.setLayoutData(composite6LData);
 				composite6.setLayout(composite6Layout);
 				{
 					composite3 = new Composite(composite6, SWT.NONE);
 					FillLayout composite3Layout = new FillLayout(org.eclipse.swt.SWT.HORIZONTAL);
 					GridData composite3LData = new GridData();
-					composite3LData.heightHint = 255;
 					composite3LData.horizontalAlignment = GridData.FILL;
 					composite3LData.grabExcessHorizontalSpace = true;
+					composite3LData.verticalAlignment = GridData.BEGINNING;
+					composite3LData.grabExcessVerticalSpace = true;
+					composite3LData.heightHint = 318;
 					composite3.setLayoutData(composite3LData);
 					composite3.setLayout(composite3Layout);
 					{
 						cTabFolder1 = new CTabFolder(composite3, SWT.NONE);
 						{
 							cTabItem1 = new CTabItem(cTabFolder1, SWT.NONE);
-							cTabItem1.setText("cTabItem1");
-							
+							cTabItem1.setText("Filter Callers");
 							{
-								sashForm1 = new SashForm(cTabFolder1, SWT.NONE);
-								GridLayout sashForm1Layout = new GridLayout();
-								sashForm1Layout.makeColumnsEqualWidth = true;
-								sashForm1.setLayout(sashForm1Layout);
-								sashForm1.setSize(350, 180);
-								cTabItem1.setControl(sashForm1);
+								groupLeft = new Group(cTabFolder1, SWT.NONE);
+								groupLeft.setText("Clases Llamadoras a Excluir");
+								
 								{
-									composite1 = new Composite(sashForm1, SWT.NONE);
-									GridLayout composite1Layout = new GridLayout();
-									composite1Layout.makeColumnsEqualWidth = true;
-									composite1.setLayout(composite1Layout);
-									GridData composite1LData = new GridData();
-									composite1.setLayoutData(composite1LData);
-									//composite1.setBackground(SWTResourceManager.getColor(255, 255, 255));
-									{
-										labelLlamadores = new CLabel(composite1, SWT.NONE);
-										labelLlamadores
-										.setText("Seleccione las clases llamadoras a excluir del análisis:");
-									//	labelLlamadores.setBackground(SWTResourceManager.getColor(255, 255, 255));
-										
+									
+									treeViewerLlamadoras = new CheckboxTreeViewer(groupLeft, SWT.NONE | SWT.BORDER);
 
-										GridData treeViewer1LData = new GridData();
-										treeViewer1LData.horizontalAlignment = GridData.FILL;
-										treeViewer1LData.grabExcessVerticalSpace = true;
-										treeViewer1LData.verticalAlignment = GridData.FILL;
-										treeViewer1LData.grabExcessHorizontalSpace = true;
-										treeViewerLlamadoras = new CheckboxTreeViewer(composite1, SWT.NONE);
-										treeViewerLlamadoras.getControl().setLayoutData(treeViewer1LData);
+									treeViewerLlamadoras
+									.setContentProvider(new ProjectTreeContentProvider(
+											project));
+									treeViewerLlamadoras
+									.setLabelProvider(new ProjectTreeLabelProvider());
+									GridData treeViewerLlamadorasLData = new GridData();
+									treeViewerLlamadorasLData.horizontalAlignment = GridData.FILL;
+									treeViewerLlamadorasLData.grabExcessHorizontalSpace = true;
+									treeViewerLlamadorasLData.verticalAlignment = GridData.FILL;
+									treeViewerLlamadorasLData.grabExcessVerticalSpace = true;
+									treeViewerLlamadoras.getControl().setLayoutData(treeViewerLlamadorasLData);
+									treeViewerLlamadoras.setInput("root");
+									treeViewerLlamadoras
+									.addCheckStateListener(new ICheckStateListener() {
 										
-										treeViewerLlamadoras
-										.setContentProvider(new ProjectTreeContentProvider(
-												project));
-										treeViewerLlamadoras
-										.setLabelProvider(new ProjectTreeLabelProvider());
-										treeViewerLlamadoras.setInput("root");
-										treeViewerLlamadoras
-										.addCheckStateListener(new ICheckStateListener() {
+										@Override
+										public void checkStateChanged(
+												CheckStateChangedEvent event) {
 											
-											@Override
-											public void checkStateChanged(
-													CheckStateChangedEvent event) {
-												
-												treeViewerLlamadoras
-												.setSubtreeChecked(
-														event
-														.getElement(),
-														event
-														.getChecked());
-												ProjectTreeContentProvider projectTreeContentProvider = (ProjectTreeContentProvider) treeViewerLlamadoras
-												.getContentProvider();
-												checkPath(event.getElement(),
-														treeViewerLlamadoras,
-														projectTreeContentProvider);
-												
-											}
+											treeViewerLlamadoras
+											.setSubtreeChecked(
+													event
+													.getElement(),
+													event
+													.getChecked());
+											ProjectTreeContentProvider projectTreeContentProvider = (ProjectTreeContentProvider) treeViewerLlamadoras
+											.getContentProvider();
+											checkPath(event.getElement(),
+													treeViewerLlamadoras,
+													projectTreeContentProvider);
 											
-										});
+										}
 										
-									}
+									});
 									
 								}
-								{
-									composite4 = new Composite(sashForm1, SWT.NONE);
-									FormLayout composite4Layout = new FormLayout();
-									GridData composite4LData = new GridData();
-									composite4.setLayoutData(composite4LData);
-									composite4.setLayout(composite4Layout);
-									{
-										Umbral = new Label(composite4, SWT.NONE);
-										FormData UmbralLData = new FormData();
-										UmbralLData.width = 61;
-										UmbralLData.height = 15;
-										UmbralLData.left =  new FormAttachment(0, 1000, 66);
-										UmbralLData.top =  new FormAttachment(0, 1000, 87);
-										Umbral.setLayoutData(UmbralLData);
-										Umbral.setText("Umbral");
-									}
-								//	composite4.setBackground(SWTResourceManager.getColor(255, 255, 255));
-									{
-										umbralText = new Text(composite4, SWT.NONE);
-										FormData UmbralLData = new FormData();
-										UmbralLData.width = 39;
-										UmbralLData.height = 15;
-										UmbralLData.left =  new FormAttachment(0, 1000, 154);
-										UmbralLData.top =  new FormAttachment(0, 1000, 87);
-										umbralText.setLayoutData(UmbralLData);
-										umbralText.setText("5");
-									}
-									{
-										FilterGetersSetter = new Button(composite4, SWT.CHECK | SWT.LEFT);
-										FormData FilterGetersSetterLData = new FormData();
-										FilterGetersSetterLData.width = 157;
-										FilterGetersSetterLData.height = 32;
-										FilterGetersSetterLData.right =  new FormAttachment(755, 1000, 0);
-										FilterGetersSetterLData.bottom =  new FormAttachment(326, 1000, 0);
-										FilterGetersSetterLData.left =  new FormAttachment(0, 1000, 66);
-										FilterGetersSetterLData.top =  new FormAttachment(0, 1000, 43);
-										FilterGetersSetter.setLayoutData(FilterGetersSetterLData);
-										FilterGetersSetter.setText("Filter Geters and Setter");
-										FilterGetersSetter.setBounds(66, 43, 157, 32);
-										//FilterGetersSetter.setBackground(SWTResourceManager.getColor(255, 255, 255));
-									}
-								}
+								cTabItem1.setControl(groupLeft);
+								GridLayout composite1Layout = new GridLayout();
+								composite1Layout.makeColumnsEqualWidth = true;
+								groupLeft.setLayout(composite1Layout);
+								//composite1.setBackground(SWTResourceManager.getColor(255, 255, 255));
+
 							}
+							
 							
 						}
 						{
 							cTabItem2 = new CTabItem(cTabFolder1, SWT.NONE);
-							cTabItem2.setText("cTabItem2");
+							cTabItem2.setText("Filters Callees");
 							{
-								composite2 = new Composite(cTabFolder1, SWT.NONE);
-								GridLayout composite2Layout = new GridLayout();
-								composite2Layout.makeColumnsEqualWidth = true;
-								composite2.setLayout(composite2Layout);
-								cTabItem2.setControl(composite2);
-							//	composite2.setBackground(SWTResourceManager.getColor(255, 255, 255));
+								groupCallees = new Group(cTabFolder1, SWT.NONE);
+								GridLayout groupCalleesLayout = new GridLayout();
+								groupCalleesLayout.makeColumnsEqualWidth = true;
+								groupCallees.setLayout(groupCalleesLayout);
+								cTabItem2.setControl(groupCallees);
+								groupCallees.setText("Clases Llamadas a Excluir:");
+								{
+									treeViewerLlamadas = new CheckboxTreeViewer(groupCallees, SWT.NONE | SWT.BORDER);
 
-								{
-									labelLlamados = new CLabel(composite2, SWT.NONE);
-									GridData cLabel1LData = new GridData();
-									labelLlamados.setLayoutData(cLabel1LData);
-									labelLlamados
-									.setText("Seleccione las clases a ser llamadas a excluir del análisis:");
-									//labelLlamados.setBackground(SWTResourceManager.getColor(255, 255, 255));
-								}
-								{
-									GridData checkboxTreeViewer1LData = new GridData();
-									checkboxTreeViewer1LData.verticalAlignment = GridData.FILL;
-									checkboxTreeViewer1LData.horizontalAlignment = GridData.FILL;
-									checkboxTreeViewer1LData.grabExcessHorizontalSpace = true;
-									checkboxTreeViewer1LData.grabExcessVerticalSpace = true;
-									treeViewerLlamadas = new CheckboxTreeViewer(
-											composite2, SWT.NONE);
-									treeViewerLlamadas.getControl().setLayoutData(checkboxTreeViewer1LData);
-									
 									treeViewerLlamadas
 									.setContentProvider(new ProjectTreeContentProvider(
 											project));
 									treeViewerLlamadas
 									.setLabelProvider(new ProjectTreeLabelProvider());
+									GridData treeViewerLlamadasLData = new GridData();
+									treeViewerLlamadasLData.horizontalAlignment = GridData.FILL;
+									treeViewerLlamadasLData.grabExcessHorizontalSpace = true;
+									treeViewerLlamadasLData.verticalAlignment = GridData.FILL;
+									treeViewerLlamadasLData.grabExcessVerticalSpace = true;
+									treeViewerLlamadas.getControl().setLayoutData(treeViewerLlamadasLData);
 									treeViewerLlamadas.setInput("root");
 									treeViewerLlamadas
 									.addCheckStateListener(new ICheckStateListener() {
@@ -277,41 +262,22 @@ public class SettingsDialogs extends org.eclipse.swt.widgets.Dialog {
 				}
 				{
 					composite5 = new Composite(composite6, SWT.NONE);
-					FormLayout composite5Layout = new FormLayout();
+					GridLayout composite5Layout = new GridLayout();
+					composite5Layout.numColumns = 2;
 					GridData composite5LData = new GridData();
 					composite5LData.heightHint = 57;
 					composite5LData.horizontalAlignment = GridData.FILL;
-					composite5LData.grabExcessHorizontalSpace = true;
+					composite5LData.verticalAlignment = GridData.END;
+					composite5LData.grabExcessVerticalSpace = true;
 					composite5.setLayoutData(composite5LData);
 					composite5.setLayout(composite5Layout);
 					{
-						CancelButton = new Button(composite5, SWT.PUSH | SWT.CENTER);
-						FormData cancelButtonLData = new FormData();
-						cancelButtonLData.width = 80;
-						cancelButtonLData.height = 29;
-						cancelButtonLData.left =  new FormAttachment(0, 1000, 508);
-						cancelButtonLData.right =  new FormAttachment(980, 1000, 0);
-						cancelButtonLData.top =  new FormAttachment(289, 1000, 0);
-						cancelButtonLData.bottom =  new FormAttachment(798, 1000, 0);
-						CancelButton.setLayoutData(cancelButtonLData);
-						CancelButton.setText("Cancelar");
-						CancelButton.addSelectionListener(new SelectionAdapter() {
-							public void widgetSelected(SelectionEvent evt) {
-								System.out.println("okButton.widgetSelected, event=" + evt);
-								// TODO add your code for okButton.widgetSelected
-								dialogShell.dispose();
-							}
-						});
-					}
-					{
 						okButton = new Button(composite5, SWT.PUSH | SWT.CENTER);
-						FormData okButtonLData = new FormData();
-						okButtonLData.width = 55;
-						okButtonLData.height = 29;
-						okButtonLData.left =  new FormAttachment(735, 1000, 0);
-						okButtonLData.right =  new FormAttachment(1000, 1000, -104);
-						okButtonLData.top =  new FormAttachment(289, 1000, 0);
-						okButtonLData.bottom =  new FormAttachment(798, 1000, 0);
+						GridData okButtonLData = new GridData();
+						okButtonLData.horizontalAlignment = GridData.END;
+						okButtonLData.grabExcessHorizontalSpace = true;
+						okButtonLData.widthHint = 60;
+						okButtonLData.heightHint = 33;
 						okButton.setLayoutData(okButtonLData);
 						okButton.setText("OK");
 						okButton.addSelectionListener(new SelectionAdapter() {
@@ -325,12 +291,28 @@ public class SettingsDialogs extends org.eclipse.swt.widgets.Dialog {
 							}
 						});
 					}
+					{
+						CancelButton = new Button(composite5, SWT.PUSH | SWT.CENTER);
+						GridData cancelButtonLData = new GridData();
+						cancelButtonLData.horizontalAlignment = GridData.END;
+						cancelButtonLData.widthHint = 60;
+						cancelButtonLData.heightHint = 33;
+						CancelButton.setLayoutData(cancelButtonLData);
+						CancelButton.setText("Cancelar");
+						CancelButton.addSelectionListener(new SelectionAdapter() {
+							public void widgetSelected(SelectionEvent evt) {
+								System.out.println("okButton.widgetSelected, event=" + evt);
+								dialogShell.dispose();
+							}
+						});
+					}
 				}
 			}
 
 			dialogShell.layout();
+			dialogShell.setSize(490,500);
 			dialogShell.pack();
-			dialogShell.setLocation(getParent().toDisplay(100, 100));
+		//	dialogShell.setLocation(getParent().toDisplay(100, 100));
 			dialogShell.open();
 			Display display = dialogShell.getDisplay();
 			while (!dialogShell.isDisposed()) {

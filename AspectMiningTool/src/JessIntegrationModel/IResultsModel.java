f8b27239d6473517bede1f4c98c06898b48db4db
luciamasola <luciamasola@3faa3960-6280-11de-987a-95e5a5a5d5f1>
2009-09-13 21:50:25 +0000

    Se agrega método generateArchive(..

--- AspectMiningTool/src/JessIntegrationModel/IResultsModel.java
+++ AspectMiningTool/src/JessIntegrationModel/IResultsModel.java
@@ -1,8 +1,12 @@
 package JessIntegrationModel;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
 public interface IResultsModel {
 
 	ProjectModel getProjectModel();
 	String getId();
+	void generateArchive(BufferedWriter archive);
 	
 }

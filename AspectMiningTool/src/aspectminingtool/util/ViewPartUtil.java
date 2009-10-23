db9fb7c79adcece06cfafd9e259c4c2d0d772cfa
luciamasola <luciamasola@3faa3960-6280-11de-987a-95e5a5a5d5f1>
2009-10-23 04:01:29 +0000

    Se crea el método openResource para abrir una clase (dado su class_id)

--- AspectMiningTool/src/aspectminingtool/util/ViewPartUtil.java
+++ AspectMiningTool/src/aspectminingtool/util/ViewPartUtil.java
@@ -3,8 +3,25 @@ package aspectminingtool.util;
 
 import java.util.List;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.ui.IWorkbenchActionConstants;
+import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.ide.IDE;
+import org.eclipse.ui.part.ViewPart;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.GroupMarker;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.action.IMenuListener;
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.action.MenuManager;
+import org.eclipse.jface.action.Separator;
 
 import JessIntegrationModel.Method;
 import JessIntegrationModel.ProjectModel;
@@ -27,5 +44,30 @@ public class ViewPartUtil {
 		}
 		
 	}
+	
+/**
+ * Abre una determinada clase resourceName de un proyecto dado projectModel
+ * @param resourceName
+ * @param projectModel
+ */
+	public static void openResource(String resourceName, ProjectModel projectModel){
+		
+
+		IResource resource = projectModel.getAssociatedResource(resourceName);
+		if (resource != null){
+			IFile fileStore = ResourcesPlugin.getWorkspace().getRoot().getFile(resource.getFullPath());
+            IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+            try {
+				IDE.openEditor(page, fileStore,true);
+			} catch (PartInitException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
+		
+	
 	
 }

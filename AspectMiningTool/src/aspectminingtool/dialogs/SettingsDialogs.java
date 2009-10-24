fff6bd3c4c3fb74dfde936fc2b4c523a0037299c
luciamasola <luciamasola@3faa3960-6280-11de-987a-95e5a5a5d5f1>
2009-10-24 03:29:29 +0000

    Se arregla la palabra configuración.

--- AspectMiningTool/src/aspectminingtool/dialogs/SettingsDialogs.java
+++ AspectMiningTool/src/aspectminingtool/dialogs/SettingsDialogs.java
@@ -85,7 +85,7 @@ public class SettingsDialogs extends org.eclipse.swt.widgets.Dialog {
 			dialogShell.setDragDetect(false);
 			{
 				composite1 = new Group(dialogShell, SWT.NONE);
-				composite1.setText("Configuracin");
+				composite1.setText("Configuración");
 				GridLayout composite1Layout = new GridLayout();
 				composite1Layout.numColumns = 2;
 				GridData composite1LData = new GridData();

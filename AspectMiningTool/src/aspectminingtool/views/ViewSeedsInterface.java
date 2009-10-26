db92caa3ffa807e6d787eb3913abc1db643136b6
luciamasola <luciamasola@3faa3960-6280-11de-987a-95e5a5a5d5f1>
2009-10-26 22:49:02 +0000

    Cambio en los parámetros del metodo.

--- AspectMiningTool/src/aspectminingtool/views/ViewSeedsInterface.java
+++ AspectMiningTool/src/aspectminingtool/views/ViewSeedsInterface.java
@@ -2,13 +2,16 @@ package aspectminingtool.views;
 
 import java.util.List;
 
+import aspectminingtool.JessIntegrationModel.GeneralSeeds.RelatedMethodDescription;
+
 import JessIntegrationModel.Method;
 import JessIntegrationModel.ProjectModel;
-import aspectminingtool.model.Call_Counted;
 
 public interface ViewSeedsInterface {
 
-	void addMethodToModel(Method method, List<Call_Counted> list,
-			ProjectModel pm);
 	
+	void addSeedToModel(Method method, String algorithm, List<RelatedMethodDescription> relatedMethods,
+			ProjectModel projectModel);
+	
+		
 }

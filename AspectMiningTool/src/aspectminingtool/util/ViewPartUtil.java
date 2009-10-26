1c5df10b2ebad25f64207bc4e3c427fa5595a831
luciamasola <luciamasola@3faa3960-6280-11de-987a-95e5a5a5d5f1>
2009-10-26 22:47:51 +0000

    Se modifica el método selectAsSeed para reconocer los distintos sistemas que se están analizando.

--- AspectMiningTool/src/aspectminingtool/util/ViewPartUtil.java
+++ AspectMiningTool/src/aspectminingtool/util/ViewPartUtil.java
@@ -6,37 +6,63 @@ import java.util.List;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.jface.viewers.TableViewer;
-import org.eclipse.swt.widgets.Menu;
-import org.eclipse.ui.IWorkbenchActionConstants;
+import org.eclipse.ui.IViewReference;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.ide.IDE;
-import org.eclipse.ui.part.ViewPart;
-
-import org.eclipse.jface.action.Action;
-import org.eclipse.jface.action.GroupMarker;
-import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.action.IMenuListener;
-import org.eclipse.jface.action.IMenuManager;
-import org.eclipse.jface.action.MenuManager;
-import org.eclipse.jface.action.Separator;
 
 import JessIntegrationModel.Method;
 import JessIntegrationModel.ProjectModel;
-import aspectminingtool.model.Call_Counted;
-import aspectminingtool.views.ViewSeedsInterface;
+import aspectminingtool.JessIntegrationModel.GeneralSeeds.RelatedMethodDescription;
+import aspectminingtool.views.SeedsGeneral.ViewPartSeeds;
 
 public class ViewPartUtil {
 
-	public static void selectAsSeed(Method method, String idView, List<Call_Counted> list , ProjectModel pm){
+	public static void selectAsSeed(Method method, String idView, List<RelatedMethodDescription> relatedMethods , ProjectModel projectModel, String algorithm){
 		
 		try {
-			ViewSeedsInterface view = (ViewSeedsInterface) PlatformUI.getWorkbench().getActiveWorkbenchWindow()
-			.getActivePage().showView(idView );
+//			ViewSeedsInterface view = (ViewSeedsInterface) PlatformUI.getWorkbench().getActiveWorkbenchWindow()
+//			.getActivePage().showView(idView );
+//			view.addSeedToModel(method, algorithm, relatedMethods, projectModel);
+			
+			String secondaryId = projectModel.getName();
+			
+//			ViewPartSeeds view = (ViewPartSeeds) PlatformUI.getWorkbench().getActiveWorkbenchWindow()
+//			.getActivePage().showView(ViewPartSeeds.ID_VIEW, projectModel.getName() , IWorkbenchPage.VIEW_CREATE);
+//		
+//			view.addSeedToModel(method, algorithm, relatedMethods, projectModel);
+//			PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().showView(ViewPartSeeds.ID_VIEW, projectModel.getName() , IWorkbenchPage.VIEW_ACTIVATE);
 
-			view.addMethodToModel(method, list, pm);
+
+			IViewReference[] vistas = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getViewReferences();
+			boolean found = false;
+			for ( int i = 0; i < vistas.length && !found; i++){
+				
+				if ( idView.equals(vistas[i].getId()) && secondaryId.equals(vistas[i].getSecondaryId())) {
+					
+					ViewPartSeeds viewSeeds = (ViewPartSeeds) PlatformUI.getWorkbench().getActiveWorkbenchWindow()
+					.getActivePage().showView(ViewPartSeeds.ID_VIEW, secondaryId, IWorkbenchPage.VIEW_ACTIVATE);
+					viewSeeds.addSeedToModel(method, algorithm, relatedMethods, projectModel);
+					PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().showView(ViewPartSeeds.ID_VIEW, secondaryId , IWorkbenchPage.VIEW_ACTIVATE);
+					found = true;
+					
+				}
+					
+			}
+			
+			if (!found){
+				
+			
+				ViewPartSeeds view = (ViewPartSeeds) PlatformUI.getWorkbench().getActiveWorkbenchWindow()
+				.getActivePage().showView(ViewPartSeeds.ID_VIEW, projectModel.getName() , IWorkbenchPage.VIEW_CREATE);
+				view.setName(secondaryId);
+				view.addSeedToModel(method, algorithm, relatedMethods, projectModel);
+				PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().showView(ViewPartSeeds.ID_VIEW, projectModel.getName() , IWorkbenchPage.VIEW_ACTIVATE);
+				
+			}
+			
+		
 			
 		} catch (PartInitException e) {
 			// TODO Auto-generated catch block

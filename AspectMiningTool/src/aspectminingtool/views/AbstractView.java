27ffc6770d1a70cf860f5338e9b7b70495204dae
luciamasola <luciamasola@3faa3960-6280-11de-987a-95e5a5a5d5f1>
2009-10-27 02:18:14 +0000

    La funcionaldiad de buscar pasa a otro objeto.

--- AspectMiningTool/src/aspectminingtool/views/AbstractView.java
+++ AspectMiningTool/src/aspectminingtool/views/AbstractView.java
@@ -3,7 +3,6 @@ package aspectminingtool.views;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.swt.custom.CLabel;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Table;
@@ -17,18 +16,14 @@ import aspectminingtool.JessIntegrationModel.Results;
 public abstract class AbstractView extends ViewPart{
 
 	protected IResultsModel model;
-	protected TableViewer tableViewerLeft;
-	protected Table tableLeft;
-	protected Table tableRight;
-	protected TableViewer tableViewerRight;
 	
-	protected Button buttonSearch;
-    protected CLabel labelSearch;
-    protected Text textSearch;
-    
-	protected String searchHistory = "";
-	protected int searchCounted = 0;
-	protected List<TableItem> searchHistoryResults;
+//	protected Button buttonSearch;
+//    protected CLabel labelSearch;
+//    protected Text textSearch;
+//    
+//	protected String searchHistory = "";
+//	protected int searchCounted = 0;
+//	protected List<TableItem> searchHistoryResults;
     
 	
 	
@@ -36,40 +31,42 @@ public abstract class AbstractView extends ViewPart{
 		return model;
 	}
 	
-	public void locateItemInTable() {
-
-		String searchClass = textSearch.getText().toLowerCase();
-		if (!searchClass.equals("")){
-			//si tengo que buscar el siguiente Item, lo busco en el searchHistory
-			if (searchHistory.equals(searchClass)){
-				
-				if (searchCounted >= searchHistoryResults.size()-1)
-					searchCounted = 0;
-				else
-					searchCounted++;
-				
-				
-			}
-			//sino, busco todos los valores y lleno el history
-			else
-			{
-				searchHistory = searchClass;
-				searchCounted = 0;
-				searchHistoryResults = new ArrayList<TableItem>();
 	
-				TableItem[] items = tableLeft.getItems();
 	
-				for (int i = 0 ; i < items.length; i++){
-					if (searchClass.equals(((Results)items[i].getData()).getSearchData())){
-						searchHistoryResults.add(items[i]);
-					}
-				}
-			}
-			if (searchHistoryResults.size() > 0)
-				tableLeft.setSelection(searchHistoryResults.get(searchCounted));
-		}
-		
-	}
+//	public void locateItemInTable(Table tableLeft, String searchHistory, int searchCounted,List<TableItem> searchHistoryResults) {
+//
+//		String searchClass = textSearch.getText().toLowerCase();
+//		if (!searchClass.equals("")){
+//			//si tengo que buscar el siguiente Item, lo busco en el searchHistory
+//			if (searchHistory.equals(searchClass)){
+//				
+//				if (searchCounted >= searchHistoryResults.size()-1)
+//					searchCounted = 0;
+//				else
+//					searchCounted++;
+//				
+//				
+//			}
+//			//sino, busco todos los valores y lleno el history
+//			else
+//			{
+//				searchHistory = searchClass;
+//				searchCounted = 0;
+//				searchHistoryResults = new ArrayList<TableItem>();
+//	
+//				TableItem[] items = tableLeft.getItems();
+//	
+//				for (int i = 0 ; i < items.length; i++){
+//					if (searchClass.equals(((Results)items[i].getData()).getSearchData())){
+//						searchHistoryResults.add(items[i]);
+//					}
+//				}
+//			}
+//			if (searchHistoryResults.size() > 0)
+//				tableLeft.setSelection(searchHistoryResults.get(searchCounted));
+//		}
+//		
+//	}
 	
 	
 	

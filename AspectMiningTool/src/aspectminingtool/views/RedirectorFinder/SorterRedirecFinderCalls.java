0666f2ab1b56d392a8ed7a5c6a14713d40dcd5c4
luciamasola <luciamasola@3faa3960-6280-11de-987a-95e5a5a5d5f1>
2009-10-28 20:44:45 +0000

    Sorter para la segunda tabla.

---
+++ AspectMiningTool/src/aspectminingtool/views/RedirectorFinder/SorterRedirecFinderCalls.java
@@ -0,0 +1,50 @@
+package aspectminingtool.views.RedirectorFinder;
+
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerSorter;
+
+import aspectminingtool.model.Call_Counted;
+
+public class SorterRedirecFinderCalls extends ViewerSorter {
+	private static final int ASCENDING = 0;
+	  private static final int DESCENDING = 1;
+	  private int column;
+	  private int direction;
+	@Override
+	public int category(Object element) {
+		// TODO Auto-generated method stub
+		return super.category(element);
+	}
+	 public void doSort(int column) {
+		    if (column == this.column) {
+		      direction = 1 - direction;
+		    } else {
+		      this.column = column;
+		      direction = ASCENDING;
+		    }
+		  }
+	
+	@Override
+	public int compare(Viewer viewer, Object e1, Object e2) {
+		int rc = 0;
+		Call_Counted cc1 = (Call_Counted)e1;
+		Call_Counted cc2 = (Call_Counted)e2;
+		switch (column) {
+	    case 0:
+    	String caller1 = cc1.toString();
+    	String caller2 = cc2.toString();
+    	rc = caller1.compareTo(caller2);
+      break;	    
+	    }
+		    
+		if (direction == DESCENDING)
+		      rc = -rc;
+	    return rc;
+	}
+
+	@Override
+	public void sort(Viewer viewer, Object[] elements) {
+		super.sort(viewer, elements);
+	}
+
+}

4b5127aec2858bee005acaebd614327d4564068f
luciamasola <luciamasola@3faa3960-6280-11de-987a-95e5a5a5d5f1>
2009-11-02 03:45:38 +0000

    umbrales y confianzas desde pantalla

--- AspectMiningTool/src/aspectminingtool/dialogs/Sinergia/SinergiaSettings.java
+++ AspectMiningTool/src/aspectminingtool/dialogs/Sinergia/SinergiaSettings.java
@@ -2,10 +2,11 @@ package aspectminingtool.dialogs.Sinergia;
 
 //import com.cloudgarden.resource.SWTResourceManager;
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.dialogs.ProgressMonitorDialog;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CLabel;
 import org.eclipse.swt.events.SelectionAdapter;
@@ -20,6 +21,16 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 
 import aspectminingtool.Algorithms.Sinergia.SinergiaAnalysis;
+import aspectminingtool.JessIntegrationModel.Sinergia.ExecutionRelationTrust;
+import aspectminingtool.JessIntegrationModel.Sinergia.FanInTrust;
+import aspectminingtool.JessIntegrationModel.Sinergia.FanInUmbral;
+import aspectminingtool.JessIntegrationModel.Sinergia.InsideFirstExecutionUmbral;
+import aspectminingtool.JessIntegrationModel.Sinergia.InsideLastExecutionUmbral;
+import aspectminingtool.JessIntegrationModel.Sinergia.OutsideAfterExecutionUmbral;
+import aspectminingtool.JessIntegrationModel.Sinergia.OutsideBeforeExecutionUmbral;
+import aspectminingtool.JessIntegrationModel.Sinergia.UmbralTrust;
+import aspectminingtool.JessIntegrationModel.Sinergia.UniqueMethodTrust;
+import aspectminingtool.JessIntegrationModel.Sinergia.UniqueMethodUmbral;
 
 /**
  * This code was edited or generated using CloudGarden's Jigloo SWT/Swing GUI
@@ -42,7 +53,7 @@ public class SinergiaSettings extends org.eclipse.swt.widgets.Dialog {
 	private Text TextUmbralFanIn;
 	private CLabel LabelConfianzaFanIn;
 	private Text TextConfianzaFanIn;
-	
+
 	private Group group9;
 	private Composite composite991;
 	private Composite composite992;
@@ -50,7 +61,7 @@ public class SinergiaSettings extends org.eclipse.swt.widgets.Dialog {
 	private Text TextUmbralUniqueM;
 	private Text TextConfianzaUniqueM;
 	private CLabel LabelConfianzaUniqueM;
-	
+
 	private Group group6;
 	private Composite composite661;
 	private Composite composite662;
@@ -58,7 +69,7 @@ public class SinergiaSettings extends org.eclipse.swt.widgets.Dialog {
 	private Text TextUmbralFlowGraph;
 	private CLabel LabelConfianzaFlowGraph;
 	private Text TextConfianzaFlowGraph;
-	
+
 	private Group group0;
 	private Composite composite001;
 
@@ -72,9 +83,9 @@ public class SinergiaSettings extends org.eclipse.swt.widgets.Dialog {
 	private IJavaProject javaProject;
 
 	public SinergiaSettings(Shell parent, int style) {
-		super(parent,style);
+		super(parent, style);
 	}
-	
+
 	public SinergiaSettings(Shell parent, int style, IJavaProject project) {
 		super(parent, style);
 		this.javaProject = project;
@@ -88,11 +99,11 @@ public class SinergiaSettings extends org.eclipse.swt.widgets.Dialog {
 					| SWT.APPLICATION_MODAL);
 
 			{
-				//Register as a resource user - SWTResourceManager will
-				//handle the obtaining and disposing of resources
-			//	SWTResourceManager.registerResourceUser(dialogShell);
+				// Register as a resource user - SWTResourceManager will
+				// handle the obtaining and disposing of resources
+				// SWTResourceManager.registerResourceUser(dialogShell);
 			}
-			
+
 			GridLayout dialogShellLayout = new GridLayout();
 			dialogShellLayout.makeColumnsEqualWidth = true;
 			dialogShell.setLayout(dialogShellLayout);
@@ -112,7 +123,7 @@ public class SinergiaSettings extends org.eclipse.swt.widgets.Dialog {
 				composite1LData.heightHint = 37;
 				group1.setLayoutData(composite1LData);
 				group1.setLayout(composite1Layout);
-				
+
 				{
 					GridData compositeUmbralLData = new GridData();
 					compositeUmbralLData.horizontalAlignment = GridData.FILL;
@@ -122,8 +133,8 @@ public class SinergiaSettings extends org.eclipse.swt.widgets.Dialog {
 					compositeUmbralLayout.makeColumnsEqualWidth = true;
 					compositeUmbralLayout.numColumns = 2;
 					composite111.setLayout(compositeUmbralLayout);
-					composite111.setLayoutData(compositeUmbralLData);					
-					
+					composite111.setLayoutData(compositeUmbralLData);
+
 					{
 						LabelUmbralFanIn = new CLabel(composite111, SWT.NONE);
 						GridData labelUmbralLData = new GridData();
@@ -139,7 +150,7 @@ public class SinergiaSettings extends org.eclipse.swt.widgets.Dialog {
 						UmbralLData.widthHint = 24;
 						UmbralLData.heightHint = 15;
 						TextUmbralFanIn.setLayoutData(UmbralLData);
-						TextUmbralFanIn.setText("0");
+						TextUmbralFanIn.setText("10");
 					}
 				}
 				{
@@ -167,12 +178,12 @@ public class SinergiaSettings extends org.eclipse.swt.widgets.Dialog {
 						UmbralLData.widthHint = 24;
 						UmbralLData.heightHint = 15;
 						TextConfianzaFanIn.setLayoutData(UmbralLData);
-						TextConfianzaFanIn.setText("0");
+						TextConfianzaFanIn.setText("33");
 					}
 				}
 
 			}
-			
+
 			group9 = new Group(dialogShell, SWT.NONE);
 			group9.setText("Unique Methods Analysis");
 			GridLayout composite9Layout = new GridLayout();
@@ -194,53 +205,52 @@ public class SinergiaSettings extends org.eclipse.swt.widgets.Dialog {
 				composite991.setLayoutData(composite2LData);
 			}
 
- 					{
-						LabelUmbralUniqueM = new CLabel(composite991, SWT.NONE);
-						GridData LabelUmbralUniqueMLData = new GridData();
-						LabelUmbralUniqueMLData.horizontalIndent = -5;
-						LabelUmbralUniqueMLData.widthHint = 65;
-						LabelUmbralUniqueMLData.heightHint = 21;
-						LabelUmbralUniqueM.setLayoutData(LabelUmbralUniqueMLData);
-						LabelUmbralUniqueM.setText("Threshold");
-					}
-					{
-						TextUmbralUniqueM = new Text(composite991, SWT.BORDER);
-						GridData TextUmbralUniqueMLData = new GridData();
-						TextUmbralUniqueMLData.widthHint = 24;
-						TextUmbralUniqueMLData.heightHint = 15;
-						TextUmbralUniqueM.setLayoutData(TextUmbralUniqueMLData);
-						TextUmbralUniqueM.setText("0");
-					}
-					
-				{
-					GridData composite12LData = new GridData();
-					composite992 = new Composite(group9, SWT.NONE);
-					GridLayout composite12Layout = new GridLayout();
-					composite12Layout.makeColumnsEqualWidth = true;
-					composite12Layout.numColumns = 2;
-					composite992.setLayout(composite12Layout);
-					composite992.setLayoutData(composite12LData);
-				}
+			{
+				LabelUmbralUniqueM = new CLabel(composite991, SWT.NONE);
+				GridData LabelUmbralUniqueMLData = new GridData();
+				LabelUmbralUniqueMLData.horizontalIndent = -5;
+				LabelUmbralUniqueMLData.widthHint = 65;
+				LabelUmbralUniqueMLData.heightHint = 21;
+				LabelUmbralUniqueM.setLayoutData(LabelUmbralUniqueMLData);
+				LabelUmbralUniqueM.setText("Threshold");
+			}
+			{
+				TextUmbralUniqueM = new Text(composite991, SWT.BORDER);
+				GridData TextUmbralUniqueMLData = new GridData();
+				TextUmbralUniqueMLData.widthHint = 24;
+				TextUmbralUniqueMLData.heightHint = 15;
+				TextUmbralUniqueM.setLayoutData(TextUmbralUniqueMLData);
+				TextUmbralUniqueM.setText("10");
+			}
 
-		 					{
-								LabelConfianzaUniqueM = new CLabel(composite992, SWT.NONE);
-								GridData LabelConfianzaUniqueMLData = new GridData();
-								LabelConfianzaUniqueMLData.horizontalIndent = -5;
-								LabelConfianzaUniqueMLData.widthHint = 40;
-								LabelConfianzaUniqueMLData.heightHint = 21;
-								LabelConfianzaUniqueM.setLayoutData(LabelConfianzaUniqueMLData);
-								LabelConfianzaUniqueM.setText("Trust");
-							}
-							{
-								TextConfianzaUniqueM = new Text(composite992, SWT.BORDER);
-								GridData TextConfianzaUniqueMLData = new GridData();
-								TextConfianzaUniqueMLData.widthHint = 24;
-								TextConfianzaUniqueMLData.heightHint = 15;
-								TextConfianzaUniqueM.setLayoutData(TextConfianzaUniqueMLData);
-								TextConfianzaUniqueM.setText("0");
-							}
-					
-			
+			{
+				GridData composite12LData = new GridData();
+				composite992 = new Composite(group9, SWT.NONE);
+				GridLayout composite12Layout = new GridLayout();
+				composite12Layout.makeColumnsEqualWidth = true;
+				composite12Layout.numColumns = 2;
+				composite992.setLayout(composite12Layout);
+				composite992.setLayoutData(composite12LData);
+			}
+
+			{
+				LabelConfianzaUniqueM = new CLabel(composite992, SWT.NONE);
+				GridData LabelConfianzaUniqueMLData = new GridData();
+				LabelConfianzaUniqueMLData.horizontalIndent = -5;
+				LabelConfianzaUniqueMLData.widthHint = 40;
+				LabelConfianzaUniqueMLData.heightHint = 21;
+				LabelConfianzaUniqueM.setLayoutData(LabelConfianzaUniqueMLData);
+				LabelConfianzaUniqueM.setText("Trust");
+			}
+			{
+				TextConfianzaUniqueM = new Text(composite992, SWT.BORDER);
+				GridData TextConfianzaUniqueMLData = new GridData();
+				TextConfianzaUniqueMLData.widthHint = 24;
+				TextConfianzaUniqueMLData.heightHint = 15;
+				TextConfianzaUniqueM.setLayoutData(TextConfianzaUniqueMLData);
+				TextConfianzaUniqueM.setText("33");
+			}
+
 			{
 				group6 = new Group(dialogShell, SWT.NONE);
 				group6.setText("Flow Graph Analysis");
@@ -253,35 +263,36 @@ public class SinergiaSettings extends org.eclipse.swt.widgets.Dialog {
 				composite6LData.grabExcessHorizontalSpace = true;
 				group6.setLayoutData(composite6LData);
 				group6.setLayout(composite6Layout);
-				
- {
-				GridData composite3LData = new GridData();
-				composite661 = new Composite(group6, SWT.NONE);
-				GridLayout composite3Layout = new GridLayout();
-				composite3Layout.makeColumnsEqualWidth = true;
-				composite3Layout.numColumns = 2;
-				composite661.setLayout(composite3Layout);
-				composite661.setLayoutData(composite3LData);
-			}
 
- 					{
-						LabelUmbralFlowGraph = new CLabel(composite661, SWT.NONE);
-						GridData LabelUmbralFlowGraphLData = new GridData();
-						LabelUmbralFlowGraphLData.horizontalIndent = -5;
-						LabelUmbralFlowGraphLData.widthHint = 65;
-						LabelUmbralFlowGraphLData.heightHint = 21;
-						LabelUmbralFlowGraph.setLayoutData(LabelUmbralFlowGraphLData);
-						LabelUmbralFlowGraph.setText("Threshold");
-					}
-					{
-						
-						TextUmbralFlowGraph = new Text(composite661, SWT.BORDER);
-						GridData TextUmbralFlowGraphLData = new GridData();
-						TextUmbralFlowGraphLData.widthHint = 24;
-						TextUmbralFlowGraphLData.heightHint = 15;
-						TextUmbralFlowGraph.setLayoutData(TextUmbralFlowGraphLData);
-						TextUmbralFlowGraph.setText("0");
-					}
+				{
+					GridData composite3LData = new GridData();
+					composite661 = new Composite(group6, SWT.NONE);
+					GridLayout composite3Layout = new GridLayout();
+					composite3Layout.makeColumnsEqualWidth = true;
+					composite3Layout.numColumns = 2;
+					composite661.setLayout(composite3Layout);
+					composite661.setLayoutData(composite3LData);
+				}
+
+				{
+					LabelUmbralFlowGraph = new CLabel(composite661, SWT.NONE);
+					GridData LabelUmbralFlowGraphLData = new GridData();
+					LabelUmbralFlowGraphLData.horizontalIndent = -5;
+					LabelUmbralFlowGraphLData.widthHint = 65;
+					LabelUmbralFlowGraphLData.heightHint = 21;
+					LabelUmbralFlowGraph
+							.setLayoutData(LabelUmbralFlowGraphLData);
+					LabelUmbralFlowGraph.setText("Threshold");
+				}
+				{
+
+					TextUmbralFlowGraph = new Text(composite661, SWT.BORDER);
+					GridData TextUmbralFlowGraphLData = new GridData();
+					TextUmbralFlowGraphLData.widthHint = 24;
+					TextUmbralFlowGraphLData.heightHint = 15;
+					TextUmbralFlowGraph.setLayoutData(TextUmbralFlowGraphLData);
+					TextUmbralFlowGraph.setText("10");
+				}
 
 				{
 					GridData composite13LData = new GridData();
@@ -293,26 +304,27 @@ public class SinergiaSettings extends org.eclipse.swt.widgets.Dialog {
 					composite662.setLayoutData(composite13LData);
 				}
 
-	 					{
-							LabelConfianzaFlowGraph = new CLabel(composite662, SWT.NONE);
-							GridData LabelConfianzaFlowGraphLData = new GridData();
-							LabelConfianzaFlowGraphLData.horizontalIndent = -5;
-							LabelConfianzaFlowGraphLData.widthHint = 40;
-							LabelConfianzaFlowGraphLData.heightHint = 21;
-							LabelConfianzaFlowGraph.setLayoutData(LabelConfianzaFlowGraphLData);
-							LabelConfianzaFlowGraph.setText("Trust");
-						}
-						{
-							
-							TextConfianzaFlowGraph = new Text(composite662, SWT.BORDER);
-							GridData TextUmbralFlowGraphLData = new GridData();
-							TextUmbralFlowGraphLData.widthHint = 24;
-							TextUmbralFlowGraphLData.heightHint = 15;
-							TextConfianzaFlowGraph.setLayoutData(TextUmbralFlowGraphLData);
-							TextConfianzaFlowGraph.setText("0");
-						}
-								
-				
+				{
+					LabelConfianzaFlowGraph = new CLabel(composite662, SWT.NONE);
+					GridData LabelConfianzaFlowGraphLData = new GridData();
+					LabelConfianzaFlowGraphLData.horizontalIndent = -5;
+					LabelConfianzaFlowGraphLData.widthHint = 40;
+					LabelConfianzaFlowGraphLData.heightHint = 21;
+					LabelConfianzaFlowGraph
+							.setLayoutData(LabelConfianzaFlowGraphLData);
+					LabelConfianzaFlowGraph.setText("Trust");
+				}
+				{
+
+					TextConfianzaFlowGraph = new Text(composite662, SWT.BORDER);
+					GridData TextUmbralFlowGraphLData = new GridData();
+					TextUmbralFlowGraphLData.widthHint = 24;
+					TextUmbralFlowGraphLData.heightHint = 15;
+					TextConfianzaFlowGraph
+							.setLayoutData(TextUmbralFlowGraphLData);
+					TextConfianzaFlowGraph.setText("33");
+				}
+
 				group0 = new Group(dialogShell, SWT.NONE);
 				group0.setText("Sinergia Analysis");
 				GridLayout composite11Layout = new GridLayout();
@@ -332,28 +344,28 @@ public class SinergiaSettings extends org.eclipse.swt.widgets.Dialog {
 					composite7Layout.makeColumnsEqualWidth = true;
 					composite7Layout.numColumns = 2;
 					composite001.setLayoutData(composite7LData);
-					
+
 					{
 						LabelUmbralSinergia = new CLabel(composite001, SWT.NONE);
 						GridData LabelUmbralSinergiaLData = new GridData();
 						LabelUmbralSinergiaLData.horizontalIndent = -5;
 						LabelUmbralSinergiaLData.widthHint = 65;
 						LabelUmbralSinergiaLData.heightHint = 21;
-						LabelUmbralSinergia.setLayoutData(LabelUmbralSinergiaLData);
+						LabelUmbralSinergia
+								.setLayoutData(LabelUmbralSinergiaLData);
 						LabelUmbralSinergia.setText("Threshold");
 					}
 					{
-						
+
 						TextUmbralSinergia = new Text(composite001, SWT.BORDER);
 						GridData TextUmbralSinergiaLData = new GridData();
 						TextUmbralSinergiaLData.widthHint = 24;
 						TextUmbralSinergiaLData.heightHint = 15;
-						TextUmbralSinergia.setLayoutData(TextUmbralSinergiaLData);
-						TextUmbralSinergia.setText("0");
+						TextUmbralSinergia
+								.setLayoutData(TextUmbralSinergiaLData);
+						TextUmbralSinergia.setText("50");
 					}
 
-					
-					
 				}
 
 			}
@@ -377,31 +389,38 @@ public class SinergiaSettings extends org.eclipse.swt.widgets.Dialog {
 					okButton.setText("OK");
 					okButton.addSelectionListener(new SelectionAdapter() {
 						public void widgetSelected(SelectionEvent evt) {
-							SinergiaAnalysis sA = new SinergiaAnalysis(javaProject);
-//							sA.calculateFacts();
-//							Shell shell = new Shell();
-//
-//							try {
-//								new ProgressMonitorDialog(shell).run(true, true, sA);
-//														
-//							} catch (InvocationTargetException e) {
-//								MessageDialog.openError(shell, "Error", e.getMessage());
-//							} catch (InterruptedException e) {
-//								// TODO Auto-generated catch block
-//								 MessageDialog.openInformation(shell, "Cancelled", e.getMessage());
-//							}
+							SinergiaAnalysis sA = new SinergiaAnalysis(
+									javaProject);
+							sA.assertRestricciones(getRestricciones());
+							// sA.calculateFacts();
+							// Shell shell = new Shell();
+							//
+							// try {
+							// new ProgressMonitorDialog(shell).run(true, true,
+							// sA);
+							//														
+							// } catch (InvocationTargetException e) {
+							// MessageDialog.openError(shell, "Error",
+							// e.getMessage());
+							// } catch (InterruptedException e) {
+							// // TODO Auto-generated catch block
+							// MessageDialog.openInformation(shell, "Cancelled",
+							// e.getMessage());
+							// }
 							Shell shell = new Shell();
 							try {
 								sA.ejecutar();
-								
+
 							} catch (InvocationTargetException e) {
-								MessageDialog.openError(shell, "Error", e.getMessage());
+								MessageDialog.openError(shell, "Error", e
+										.getMessage());
 							} catch (InterruptedException e) {
-								 MessageDialog.openInformation(shell, "Cancelled", e.getMessage());
+								MessageDialog.openInformation(shell,
+										"Cancelled", e.getMessage());
 							}
-							//leer seleccion de árbol
-							//leer los checkboxs
-							//mandar a hacer filtrados
+							// leer seleccion de árbol
+							// leer los checkboxs
+							// mandar a hacer filtrados
 							dialogShell.dispose();
 						}
 					});
@@ -416,7 +435,9 @@ public class SinergiaSettings extends org.eclipse.swt.widgets.Dialog {
 					CancelButton.setText("Cancelar");
 					CancelButton.addSelectionListener(new SelectionAdapter() {
 						public void widgetSelected(SelectionEvent evt) {
-							System.out.println("okButton.widgetSelected, event=" + evt);
+							System.out
+									.println("okButton.widgetSelected, event="
+											+ evt);
 							dialogShell.dispose();
 						}
 					});
@@ -425,7 +446,7 @@ public class SinergiaSettings extends org.eclipse.swt.widgets.Dialog {
 
 			dialogShell.layout();
 			dialogShell.pack();
-		//	dialogShell.setLocation(getParent().toDisplay(100, 100));
+			// dialogShell.setLocation(getParent().toDisplay(100, 100));
 			dialogShell.open();
 			Display display = dialogShell.getDisplay();
 			while (!dialogShell.isDisposed()) {
@@ -437,6 +458,37 @@ public class SinergiaSettings extends org.eclipse.swt.widgets.Dialog {
 		}
 	}
 
-	
+	private List getRestricciones(){
+		List restriciones = new ArrayList();
+		
+		FanInTrust fit = new FanInTrust(TextConfianzaFanIn.getText());
+		restriciones.add(fit);
+		
+		FanInUmbral fiu = new FanInUmbral(TextUmbralFanIn.getText());
+		restriciones.add(fiu);
+		
+		UniqueMethodTrust umt = new UniqueMethodTrust(TextConfianzaUniqueM.getText());
+		restriciones.add(umt);
+		
+		UniqueMethodUmbral umu = new UniqueMethodUmbral(TextUmbralUniqueM.getText());
+		restriciones.add(umu);
+		
+		ExecutionRelationTrust ert = new ExecutionRelationTrust(TextConfianzaFlowGraph.getText());
+		restriciones.add(ert);
+		
+		InsideFirstExecutionUmbral ifu = new InsideFirstExecutionUmbral(TextUmbralFlowGraph.getText());
+		InsideLastExecutionUmbral ilu = new InsideLastExecutionUmbral(TextUmbralFlowGraph.getText());
+		OutsideBeforeExecutionUmbral obu = new OutsideBeforeExecutionUmbral(TextUmbralFlowGraph.getText());
+		OutsideAfterExecutionUmbral oau = new OutsideAfterExecutionUmbral(TextUmbralFlowGraph.getText());
+		restriciones.add(ert);
+		restriciones.add(ilu);
+		restriciones.add(obu);
+		restriciones.add(oau);
+		
+		UmbralTrust ut = new UmbralTrust(TextUmbralSinergia.getText());
+		restriciones.add(ut);
+		
+		return restriciones;
+	}
 
 }
